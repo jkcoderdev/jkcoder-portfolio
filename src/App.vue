62b0a46719071b85e369baba59ac9848f8ed5dfe
Jan Kozaruk <20jackoza06@gmail.com>
2025-05-12 20:12:09 +0200

    Start from scratch again

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,11 @@
 <script setup>
 import { RouterView } from 'vue-router';
-import Baner from './components/baner/Baner.vue';
-import Navbar from './components/navbar/Navbar.vue';
-import NavLink from './components/navbar/NavLink.vue';
 </script>
 
 <template>
-  <header class="header">
-    <div class="header-wrapper">
-      <Baner />
-
-      <Navbar>
-        <NavLink route="home">Home</NavLink>
-        <NavLink route="home">Projects</NavLink>
-        <NavLink route="home">About</NavLink>
-      </Navbar>
-    </div>
-  </header>
-
-  <main class="main">
-    <RouterView />
-  </main>
+  <RouterView />
 </template>
 
 <style lang="scss" scoped>
-.header {
-  width: 100%;
-  height: $header-height;
 
-  .header-wrapper {
-    width: $content-width;
-    max-width: 100%;
-    height: 100%;
-
-    margin: 0 auto;
-    padding: 0 $content-padding;
-
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-}
 </style>
